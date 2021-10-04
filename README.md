14986d3cde669ba606a44bd2b5746ecbab9c4037
Serkan ÖZAL <serkan@thundra.io>
2021-10-04 11:25:40 +0300

    Thundra Foresight ORB Plugin Initial Impl (#1)
    * Include initial orb definition
    
    * Change orb name
    
    * Add documentation
    
    Co-authored-by: Oguzhan Ozdemir <aoozdemir@live.com>

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # thundra-circleci-plugin
-Thundra CircleCI plugin
+
+A CircleCI Orb to instrument your projects with Thundra Foresight.
+
+Learn about Orbs [here](https://circleci.com/orbs/).
+## Usage
+
+Information about available parameters is listed [below](#parameters).
+
+The required parameters are the Thundra API Key and the Thundra Project ID, which can be obtained from [foresight.thundra.io](https://foresight.thundra.io/).
+
+You can learn more about Thundra at [thundra.io](https://thundra.io)
+
+Once you get the Thundra API Key, make sure to set it as a secret. A sample Orb usage would look like this:
+
+### Maven Build Example
+
+```yaml
+# Make sure to use version 2.1
+version: 2.1
+
+# ...
+
+orbs:
+  # Declare a dependency on the welcome-orb
+  # Make sure to replace <VERSION> with the latest available
+  thundra: thundra-io/thundra-foresight@<VERSION>
+
+# ...
+
+jobs:
+  maven:
+    docker:
+    - image: cimg/openjdk:11.0
+    steps:
+    - checkout
+      # Use `thundra/maven` command
+    - thundra/maven:
+        apikey: <THUNDRA_APIKEY>
+        project_id: <THUNDRA_PROJECT_ID>
+    - run:
+        command: mvn clean verify
+workflows:
+  example:
+    jobs:
+      - maven
+```
+
+### Gradle Build Example
+
+```yaml
+# Make sure to use version 2.1
+version: 2.1
+
+# ...
+
+orbs:
+  # Declare a dependency on the welcome-orb
+  # Make sure to replace <VERSION> with the latest available
+  thundra: thundra-io/thundra-foresight@<VERSION>
+
+# ...
+
+jobs:
+  gradle:
+    docker:
+      - image: cimg/openjdk:11.0
+    steps:
+      - checkout
+      # Use `thundra/gradle` command
+      - thundra/gradle:
+          apikey: <THUNDRA_APIKEY>
+          project_id: <THUNDRA_PROJECT_ID>
+      - run:
+          # Make sure to use the $THUNDRA_GRADLE_INIT_SCRIPT_PATH
+          command: ./gradlew build --init-script $THUNDRA_GRADLE_INIT_SCRIPT_PATH
+workflows:
+  example:
+    jobs:
+      - gradle
+```
+
+## Parameters
+
+| Name                  | Requirement       | Scope         | Description
+| ---                   | ---               | ---           | ---
+| apikey                | Required          | Maven, Gradle | Thundra API Key
+| project_id            | Required          | Maven, Gradle | Your project id from Thundra. Will be used to filter and classify your testruns.
+| instrumenter_version  | Optional          | Maven         | In the action itself, we use a Java script to manipulate pom.xml files. This script is released and versioned separately from the action. Hence, if there is some breaking change or specific version you want to use, you can use it by defining this parameter. You can see all the available version of our instrumenter [here](https://search.maven.org/artifact/io.thundra.plugin/thundra-agent-maven-test-instrumentation).
+| plugin_version        | Optional          | Gradle         | For Gradle, we use another plugin to instrument tests. You can see all the available version of our plugin [here](https://search.maven.org/artifact/io.thundra.plugin/thundra-gradle-test-plugin).
+| agent_version         | Optional          | Maven, Gradle  | A specific version Thundra Java Agent you want to use should be defined here. Similar to `instrumenter_version` parameter. You can see all the available version of our agent [here](https://repo.thundra.io/service/local/repositories/thundra-releases/content/io/thundra/agent/thundra-agent-bootstrap/maven-metadata.xml).
+| parent_pom_path       | Optional          | Maven          | Use if your parent pom is in somewhere other that ./pom.xml. **Must be a relative path.**
---
+++ src/scripts/maven.py
@@ -0,0 +1,109 @@
+import os
+import urllib3
+import shutil
+import subprocess
+import xml.etree.ElementTree as et
+
+from distutils.version import StrictVersion
+from typing import Optional
+
+THUNDRA_AGENT_METADATA: str = 'https://repo.thundra.io/service/local/repositories/thundra-releases/content/io/thundra/agent/thundra-agent-bootstrap/maven-metadata.xml'
+MAVEN_INSTRUMENTATION_METADATA: str = 'https://repo1.maven.org/maven2/io/thundra/plugin/thundra-agent-maven-test-instrumentation/maven-metadata.xml'
+
+
+def get_latest_version(repository: str, version: str = None) -> str:
+    http = urllib3.PoolManager()
+    response = http.request('GET', repository)
+    xml = et.fromstring(response.data)
+
+    available_versions = xml.findall('./versioning/versions/version')
+    available_versions_set = set(map(lambda x: x.text, available_versions))
+
+    latest_version = xml.find('./versioning/latest').text
+
+    if (version and (version in available_versions_set)):
+        return version
+    else:
+        return latest_version
+
+
+def instrument(instrumenter_version: str = None, agent_version: str = None):
+    agent_path: str
+    maven_instrumenter_path: str
+    http = urllib3.PoolManager()
+
+    maven_instrumenter_version: Optional[str] = get_latest_version(
+        MAVEN_INSTRUMENTATION_METADATA,
+        instrumenter_version,
+    )
+    if not maven_instrumenter_version:
+        print(
+            "> Couldn't find an available version for Thundra Maven Instrumentation script")
+        print("> Instrumentation failed!")
+        return
+    maven_instrumenter_url = f'https://repo1.maven.org/maven2/io/thundra/plugin/thundra-agent-maven-test-instrumentation/{maven_instrumenter_version}/thundra-agent-maven-test-instrumentation-{maven_instrumenter_version}.jar'
+
+    thundra_agent_version: Optional[str] = get_latest_version(
+        THUNDRA_AGENT_METADATA,
+        agent_version,
+    )
+    if not thundra_agent_version:
+        print("> Couldn't find an available version for Thundra Agent")
+        print("> Instrumentation failed!")
+        return
+    thundra_agent_url = f'https://repo.thundra.io/service/local/repositories/thundra-releases/content/io/thundra/agent/thundra-agent-bootstrap/{thundra_agent_version}/thundra-agent-bootstrap-{thundra_agent_version}.jar'
+
+    if os.environ.get('LOCAL_AGENT_PATH'):
+        agent_path = os.environ.get('LOCAL_AGENT_PATH')
+        print(f'> Using the local agent at {agent_path}')
+    else:
+        print("> Downloading the agent...")
+        agent_path = '/tmp/thundra-agent-bootstrap.jar'
+        with open(agent_path, 'wb') as out:
+            r = http.request('GET', thundra_agent_url, preload_content=False)
+            shutil.copyfileobj(r, out)
+        print(f'> Successfully downloaded the agent to {agent_path}')
+
+    print("> Downloading the maven instrumentater")
+    maven_instrumenter_path = f'/tmp/thundra-agent-maven-test-instrumentation-{maven_instrumenter_version}.jar'
+    with open(maven_instrumenter_path, 'wb') as out:
+        r = http.request('GET', maven_instrumenter_url, preload_content=False)
+        shutil.copyfileobj(r, out)
+    print(f'> Successfully downloaded the agent to {maven_instrumenter_path}')
+
+    print("> Updating pom.xml...")
+    poms = subprocess.run(
+        ['sh', '-c', f'find . -name "pom.xml" -exec echo \'{{}}\' +'], capture_output=True)
+
+    if poms.stdout:
+        subprocess.call(['java', '-jar', maven_instrumenter_path,
+                         agent_path, str(poms.stdout.strip(), 'utf-8')])
+        print("> Update to pom.xml is done")
+    else:
+        print("> Couldn't find any pom.xml files. Exiting the instrumentation step.")
+
+
+if not os.environ.get('THUNDRA_APIKEY'):
+    print('> Thundra API Key is not present. Exiting early...')
+    print('> Instrumentation failed.')
+    exit(0)
+
+if not os.environ.get('THUNDRA_AGENT_TEST_PROJECT_ID'):
+    print('> Thundra Project ID is not present. Exiting early...')
+    print('> Instrumentation failed.')
+    exit(0)
+
+
+if os.environ.get('THUNDRA_AGENT_VERSION') and (StrictVersion(os.environ.get('THUNDRA_AGENT_VERSION') < StrictVersion("2.7.0"))):
+    print(f'Thundra Java Agent prior to 2.7.0 doesn\'t work with this action')
+    exit(0)
+
+
+def run():
+    print(f'> [Thundra] Initializing the Thundra Action...')
+    print(f'> Instrumenting the application')
+    instrument(os.environ.get('THUNDRA_INSTRUMENTER_VERSION'),
+               os.environ.get('THUNDRA_AGENT_VERSION'))
+
+
+run()
